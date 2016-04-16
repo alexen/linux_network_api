9d7559003b660ac838232887e3d165c7c6b871b5
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2016-04-16 19:59:28 +0300

    Реализована функция чтения данных из stdin с прослушиванием событий на сокете (select())

--- main/echo_tcp_cli01.c
+++ main/echo_tcp_cli01.c
@@ -31,6 +31,39 @@ void input_line( FILE* istream, int sockfd )
 }
 
 
+void input_line_v2( FILE* istream, int sockfd )
+{
+     char sendline[ MAXLINE ] = { 0 };
+     char recvline[ MAXLINE ] = { 0 };
+     fd_set rset;
+
+     FD_ZERO( &rset );
+     const int istrfd = fileno( istream );
+     while( 1 )
+     {
+          FD_SET( istrfd, &rset );
+          FD_SET( sockfd, &rset );
+          const int maxfdp1 = MAX_OF( istrfd, sockfd ) + 1;
+
+          wrp_select( maxfdp1, &rset, 0, 0, 0 );
+
+          if( FD_ISSET( sockfd, &rset ) ) /* сокет готов на чтение */
+          {
+               if( readline( sockfd, recvline, MAXLINE ) == 0 )
+                    err_quit( "input_line: server terminated prematurely" );
+               wrp_fputs( recvline, stdout );
+          }
+
+          if( FD_ISSET( istrfd, &rset ) ) /* входной поток готов на чтение */
+          {
+               if( fgets( sendline, MAXLINE, istream ) == 0 )
+                    return;
+               write_n( sockfd, sendline, strlen( sendline ) );
+          }
+     }
+}
+
+
 int main( int ac, char** av )
 {
      if( ac != 2 )
@@ -41,13 +74,10 @@ int main( int ac, char** av )
      const int sockfd = wrp_socket( AF_INET, SOCK_STREAM, 0 );
 
      struct sockaddr_in servaddr = { 0 };
-     servaddr.sin_family = AF_INET;
-     servaddr.sin_port = htons( SERV_PORT );
-     wrp_inet_pton( AF_INET, av[ 1 ], &servaddr.sin_addr );
-
+     wrp_set_sockaddr_v4( &servaddr, av[ 1 ], SERV_PORT );
      wrp_connect( sockfd, (struct sockaddr*) &servaddr, sizeof( servaddr ) );
 
-     input_line( stdin, sockfd );
+     input_line_v2( stdin, sockfd );
 
      exit( EXIT_SUCCESS );
 }
