cc191d956b60533936f27768b2e8cac972265c37
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2018-04-07 21:04:17 +0300

    error: Добавлен тип обработчика ошибок и макросы для вызова

--- error/error.h
+++ error/error.h
@@ -5,8 +5,31 @@
  *
  */
 
-#ifndef ERROR_ERROR_H_
-#define ERROR_ERROR_H_
+#ifndef LINUX_NETWORK_API_ERROR_ERROR_H_
+#define LINUX_NETWORK_API_ERROR_ERROR_H_
+
+
+/*
+ * Тип колбека для обработки ошибок
+ *
+ * @param file имя файла, в котором произошла ошибка
+ * @param line номер строки, в которой произошла ошибка
+ * @param func имя функции, в которой произошла ошибка
+ * @param message сообщение об ошибке
+ */
+typedef void( *error_handler_t )(
+     const char* file,
+     int line,
+     const char* func,
+     const char* message
+     );
+
+
+#define ERROR_OCCURED_FNC_MSG( handler_name_, function_, message_ ) \
+     do{ handler_name_( __FILE__, __LINE__, function_, message_ ); } while( 0 )
+
+#define ERROR_OCCURED_MSG( handler_name_, message_ ) \
+     ERROR_OCCURED_FNC_MSG( handler_name_, __FUNCTION__, message_ )
 
 
 /*
@@ -40,4 +63,4 @@ void err_msg( const char *fmt, ... );
 void err_quit( const char *fmt, ... );
 
 
-#endif /* ERROR_ERROR_H_ */
+#endif /* LINUX_NETWORK_API_ERROR_ERROR_H_ */
